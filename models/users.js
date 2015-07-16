264b1aa44126b8a2f1c157603d3d57fcf896b6e6
Frank Odongkara <fodongkara@.com>
2015-07-16 18:44:11 +0300

    Initialized routes, models, bootstrap and static files

---
+++ models/users.js
@@ -0,0 +1,16 @@
+module.exports = {
+	add_user: function(){
+		return 0;
+	},
+	get_user: function(){
+		return 0;
+	}
+
+	get_users: function(){
+		return 0;
+	}
+
+	del_user: function(){
+		return 0;
+	}
+};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('users', { title: 'Users' });
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Register' });
 });
 
 module.exports = router;
