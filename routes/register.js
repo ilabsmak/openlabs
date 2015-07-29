9bae6db0a27c43665fdad90e7b39da9e679dcf92
mukishrita <ritamukimba@gmail.com>
2015-07-30 00:08:22 +0300

    views added

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET register page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'OpenLabs' });
+  res.render('register', { title: 'register' });
 });
 
 module.exports = router;
