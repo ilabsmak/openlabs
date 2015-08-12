0f5d588667c866e1fa4484533d8cfa6013acb9cd
mukishrita <ritamukimba@gmail.com>
2015-08-12 12:27:39 +0300

    12.08.15

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var register = require('./routes/register');
-//var login = require('./routes/login');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/register', register);
-//app.use('/login', login);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
